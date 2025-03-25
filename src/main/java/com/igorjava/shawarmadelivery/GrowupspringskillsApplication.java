74331942467a9d3d0056c209727b4787925922f3
Игорь Царёв <tsarov88@gmail.com>
2025-03-25 16:04:17 +0200

    initial commit

---
+++ src/main/java/com/igorjava/shawarmadelivery/GrowupspringskillsApplication.java
@@ -0,0 +1,18 @@
+package com.igorjava.shawarmadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowupspringskillsApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(GrowupspringskillsApplication.class, args);
+
+		System.out.println("Wow");
+
+
+	}
+}
---
+++ src/test/java/com/igorjava/shawarmadelivery/GrowupspringskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorjava.shawarmadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowupspringskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
