2d6d4c9320e49980befb8522cd2ce1773a0346a0
Игорь Царёв <tsarov88@gmail.com>
2025-03-27 18:27:34 +0200

    repo / menu item repo && user repo

---
+++ src/main/java/com/igorjava/shawarmadelivery/domain/repo/MenuItemRepo.java
@@ -0,0 +1,14 @@
+package com.igorjava.shawarmadelivery.domain.repo;
+
+import com.igorjava.shawarmadelivery.domain.model.MenuItem;
+import com.igorjava.shawarmadelivery.domain.model.MenuSection;
+
+import java.util.List;
+
+public interface MenuItemRepo {
+    MenuItem saveMenuItem(MenuItem menuItem);
+    MenuItem updateMenuItem(MenuItem menuItem);
+    MenuItem getMenuItemById(Long id);
+    List<MenuItem> getMenuItemBySection(MenuSection section);
+    void deleteMenuItem(MenuItem menuItem);
+}
