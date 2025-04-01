b42ee34ff7d4e2049812993ad87a0a86b3ea6b2b
Игорь Царёв <tsarov88@gmail.com>
2025-04-01 13:48:41 +0300

    order repo impl. java

---
+++ src/main/java/com/igorjava/shawarmadelivery/data/repoImpls/collectionFrw/OrderRepoImpl.java
@@ -0,0 +1,41 @@
+package com.igorjava.shawarmadelivery.data.repoImpls.collectionFrw;
+
+import com.igorjava.shawarmadelivery.domain.model.Order;
+import com.igorjava.shawarmadelivery.domain.model.OrderStatus;
+import com.igorjava.shawarmadelivery.domain.model.User;
+import com.igorjava.shawarmadelivery.domain.repo.OrderRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderRepoImpl implements OrderRepo {
+
+    private final List<Order> orders=new ArrayList<>();
+
+    @Override
+    public Order saveOrder(Order order) {
+        orders.add(order);
+        return order;
+    }
+
+    @Override
+    public Order updateOrder(Order order) {
+        int index=orders.indexOf(order);
+        if (index != -1) orders.set(index, order);
+        return order;
+    }
+
+    @Override
+    public List<Order> getOrdersByUser(User user) {
+        return orders.stream()
+                .filter(order -> order.getUser().getId().equals(user.getId()))
+                .toList();
+    }
+
+    @Override
+    public List<Order> getOrdersByStatus(OrderStatus orderStatus) {
+        return orders.stream()
+                .filter(order -> order.getStatus().name().equals(orderStatus.name()))
+                .toList();
+    }
+}
