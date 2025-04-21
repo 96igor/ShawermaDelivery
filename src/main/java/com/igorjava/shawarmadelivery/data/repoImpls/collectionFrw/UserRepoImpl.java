4e4e6c7db0cf46b40da519aa7f9e02735719b67b
Игорь Царёв <tsarov88@gmail.com>
2025-04-21 19:41:33 +0300

    logging.level.root

--- src/main/java/com/igorjava/shawarmadelivery/data/repoImpls/collectionFrw/UserRepoImpl.java
+++ src/main/java/com/igorjava/shawarmadelivery/data/repoImpls/collectionFrw/UserRepoImpl.java
@@ -2,17 +2,19 @@ package com.igorjava.shawarmadelivery.data.repoImpls.collectionFrw;
 
 import com.igorjava.shawarmadelivery.domain.model.User;
 import com.igorjava.shawarmadelivery.domain.repo.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
-@Repository(value = "URwAL")
+@Repository("URwAL")
 public class UserRepoImpl implements UserRepo {
 
     private final List<User> users = new ArrayList<>();
-    private final Logger logo = Logger.getLogger("UserRepoImpl");
+//    private final Logger logo = Logger.getLogger("UserRepoImpl");
+    private final Logger logo = LoggerFactory.getLogger(UserRepoImpl.class);
 
     @Override
     public User saveUser(User user) {
