1764a2c9bc1174c4dfe2b864c00b593812e00cdd
Игорь Царёв <tsarov88@gmail.com>
2025-05-05 17:16:01 +0300

    Order development

--- src/main/java/com/igorjava/shawarmadelivery/conf/ApplicationStartupRunner.java
+++ src/main/java/com/igorjava/shawarmadelivery/conf/ApplicationStartupRunner.java
@@ -22,50 +22,50 @@ public class ApplicationStartupRunner implements CommandLineRunner {
         menuItemRepo.saveMenuItem(
                 new MenuItem(1L, "Гиро в лаваше L", MenuSection.MAIN_MENU, BigDecimal.valueOf(240)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Гиро в лаваше XL", MenuSection.MAIN_MENU, BigDecimal.valueOf(290)));
+                new MenuItem(2L, "Гиро в лаваше XL", MenuSection.MAIN_MENU, BigDecimal.valueOf(290)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Гиро в лепёшке", MenuSection.MAIN_MENU, BigDecimal.valueOf(240)));
+                new MenuItem(3L, "Гиро в лепёшке", MenuSection.MAIN_MENU, BigDecimal.valueOf(240)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Гиро в пите", MenuSection.MAIN_MENU, BigDecimal.valueOf(240)));
+                new MenuItem(4L, "Гиро в пите", MenuSection.MAIN_MENU, BigDecimal.valueOf(240)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Люля кебаб на углях в лаваше", MenuSection.MAIN_MENU, BigDecimal.valueOf(330)));
+                new MenuItem(5L, "Люля кебаб на углях в лаваше", MenuSection.MAIN_MENU, BigDecimal.valueOf(330)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Люля на углях в лепёшке", MenuSection.MAIN_MENU, BigDecimal.valueOf(330)));
+                new MenuItem(6L, "Люля на углях в лепёшке", MenuSection.MAIN_MENU, BigDecimal.valueOf(330)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Чизбургер куринный", MenuSection.MAIN_MENU, BigDecimal.valueOf(230)));
+                new MenuItem(7L, "Чизбургер куринный", MenuSection.MAIN_MENU, BigDecimal.valueOf(230)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Хот-Дог", MenuSection.MAIN_MENU, BigDecimal.valueOf(150)));
+                new MenuItem(8L, "Хот-Дог", MenuSection.MAIN_MENU, BigDecimal.valueOf(150)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Блэкбургер", MenuSection.MAIN_MENU, BigDecimal.valueOf(230)));
+                new MenuItem(9L, "Блэкбургер", MenuSection.MAIN_MENU, BigDecimal.valueOf(230)));
 
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Фри L", MenuSection.SNACKS, BigDecimal.valueOf(130)));
+                new MenuItem(10L, "Фри L", MenuSection.SNACKS, BigDecimal.valueOf(130)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Фри XL", MenuSection.SNACKS, BigDecimal.valueOf(150)));
+                new MenuItem(11L, "Фри XL", MenuSection.SNACKS, BigDecimal.valueOf(150)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "По-деревенски", MenuSection.SNACKS, BigDecimal.valueOf(150)));
+                new MenuItem(12L, "По-деревенски", MenuSection.SNACKS, BigDecimal.valueOf(150)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Наггетсы", MenuSection.SNACKS, BigDecimal.valueOf(150)));
+                new MenuItem(13L, "Наггетсы", MenuSection.SNACKS, BigDecimal.valueOf(150)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Французкий Хот-Дог", MenuSection.SNACKS, BigDecimal.valueOf(150)));
+                new MenuItem(14L, "Французкий Хот-Дог", MenuSection.SNACKS, BigDecimal.valueOf(150)));
 
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Халапеньо", MenuSection.SUPPLEMENTS, BigDecimal.valueOf(40)));
+                new MenuItem(15L, "Халапеньо", MenuSection.SUPPLEMENTS, BigDecimal.valueOf(40)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Сыр", MenuSection.SUPPLEMENTS, BigDecimal.valueOf(40)));
+                new MenuItem(16L, "Сыр", MenuSection.SUPPLEMENTS, BigDecimal.valueOf(40)));
 
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Фирменный от Шефа", MenuSection.SAUCES, BigDecimal.valueOf(40)));
+                new MenuItem(17L, "Фирменный от Шефа", MenuSection.SAUCES, BigDecimal.valueOf(40)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Томатный", MenuSection.SAUCES, BigDecimal.valueOf(40)));
+                new MenuItem(18L, "Томатный", MenuSection.SAUCES, BigDecimal.valueOf(40)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Барбекю", MenuSection.SAUCES, BigDecimal.valueOf(40)));
+                new MenuItem(19L, "Барбекю", MenuSection.SAUCES, BigDecimal.valueOf(40)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Сырный", MenuSection.SAUCES, BigDecimal.valueOf(40)));
+                new MenuItem(20L, "Сырный", MenuSection.SAUCES, BigDecimal.valueOf(40)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Чесночный", MenuSection.SAUCES, BigDecimal.valueOf(40)));
+                new MenuItem(21L, "Чесночный", MenuSection.SAUCES, BigDecimal.valueOf(40)));
         menuItemRepo.saveMenuItem(
-                new MenuItem(1L, "Кисло-Сладкий", MenuSection.SAUCES, BigDecimal.valueOf(40)));
+                new MenuItem(22L, "Кисло-Сладкий", MenuSection.SAUCES, BigDecimal.valueOf(40)));
 
     }
 }
--- src/main/java/com/igorjava/shawarmadelivery/presentation/controller/OrderAndDeliveryController.java
+++ src/main/java/com/igorjava/shawarmadelivery/presentation/controller/OrderAndDeliveryController.java
@@ -2,10 +2,18 @@ package com.igorjava.shawarmadelivery.presentation.controller;
 
 import com.igorjava.shawarmadelivery.presentation.service.DeliveryService;
 import com.igorjava.shawarmadelivery.presentation.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @Controller
 public class OrderAndDeliveryController {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderAndDeliveryController.class);
 
     private final OrderService orderService;
     private final DeliveryService deliveryService;
@@ -13,5 +21,16 @@ public class OrderAndDeliveryController {
     public OrderAndDeliveryController(OrderService orderService, DeliveryService deliveryService) {
         this.orderService = orderService;
         this.deliveryService = deliveryService;
+    }
+
+    @PostMapping("/order")
+    public String showOrderForm(
+            @RequestParam List<Long> selectedItems,
+            @RequestParam List<Integer> quentites,
+            Model model
+    ){
+        log.info("selectedItems: {}", selectedItems);
+        log.info("quentites: {}", quentites);
+        return "order";
     }
 }
