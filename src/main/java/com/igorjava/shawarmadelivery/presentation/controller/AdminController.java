98512607487b254529d05fe59c74d7f6d5940d3f
Игорь Царёв <tsarov88@gmail.com>
2025-05-13 12:51:54 +0300

    ...Admin Panel

---
+++ src/main/java/com/igorjava/shawarmadelivery/presentation/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.igorjava.shawarmadelivery.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String showAdminPanel(){
+
+        return "admin";
+    }
+
+}
