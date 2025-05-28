1eeeb515e26de8a61e145e67c833b1cc0a5970b5
Игорь Царёв <tsarov88@gmail.com>
2025-05-28 06:46:08 +0300

    Localization of the order page

--- src/main/java/com/igorjava/shawarmadelivery/presentation/controller/OrderAndDeliveryController.java
+++ src/main/java/com/igorjava/shawarmadelivery/presentation/controller/OrderAndDeliveryController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.math.BigDecimal;
@@ -32,8 +33,17 @@ public class OrderAndDeliveryController {
         this.sessionInfoService = sessionInfoService;
     }
 
+    @GetMapping("/order")
+    public String showOrderForm(Model model) {
+        if (sessionInfoService.getCart() == null || sessionInfoService.getCart().isEmpty()) {
+            return "redirect:/menu";
+        }
+        model.addAttribute("sessionInfoService", sessionInfoService);
+        return "order";
+    }
+
     @PostMapping("/order")
-    public String showOrderForm(
+    public String processOrderForm(
             @RequestParam List<Long> selectedId,
             @RequestParam List<Integer> quantities,
             Model model
